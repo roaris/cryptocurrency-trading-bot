854379f2863dc2903819c9737cad66a761904f88
roaris <one0803kyopro@gmail.com>
2022-04-08 22:00:40 +0900

    candlesを返すAPIを作成

--- controllers/webserver.go
+++ controllers/webserver.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/cryptocurrency-trading-bot/models"
 )
 
 func viewChartHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +19,23 @@ func viewChartHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func candleHandler(w http.ResponseWriter, r *http.Request) {
+	candles, err := models.GetAllCandles()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	res, err := json.Marshal(candles)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
 func StartWebServer() {
 	http.HandleFunc("/chart", viewChartHandler)
+	http.HandleFunc("/api/candles", candleHandler)
 	http.ListenAndServe(":8000", nil)
 }
