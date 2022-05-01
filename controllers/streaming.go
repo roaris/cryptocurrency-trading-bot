4e721d195d3749118e4b2d4b5d476f9e183dec61
roaris <one0803kyopro@gmail.com>
2022-05-02 01:11:25 +0900

    取引の処理を追加

--- controllers/streaming.go
+++ controllers/streaming.go
@@ -1,16 +1,22 @@
 package controllers
 
 import (
+	"os"
+
 	"github.com/cryptocurrency-trading-bot/models"
+	"github.com/cryptocurrency-trading-bot/utils"
 )
 
 func Streaming() {
 	tickerChannel := make(chan models.Ticker)
+	apiClient := utils.NewAPIClient(os.Getenv("API_KEY"), os.Getenv("API_SECRET"))
+	bot := bot{apiClient, "SELL", false}
 	go models.GetRealTimeTicker(tickerChannel)
 
 	go func() {
 		for ticker := range tickerChannel {
 			models.CreateCandle(&ticker)
+			bot.trade()
 		}
 	}()
 }
--- models/event.go
+++ models/event.go
@@ -8,3 +8,12 @@ type SignalEvent struct {
 	Size      float64   `gorm:"not null" json:"size"`
 	CreatedAt time.Time `gorm:"not null" json:"created_at"`
 }
+
+func NewSignalEvent(side string, size float64) *SignalEvent {
+	return &SignalEvent{Side: side, Size: size}
+}
+
+func (s *SignalEvent) Save() error {
+	err := db.Save(s).Error
+	return err
+}
